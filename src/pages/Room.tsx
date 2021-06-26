edd81910df05fca0ec56ab0ece061d0d974803c1
Rafael Nóbrega de Lima <rafaelnobregadelima2809@gmail.com>
2021-06-26 01:51:22 -0400

    attt

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,18 @@
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { useParams } from 'react-router-dom';
 import '../styles/room.scss';
 
 export function Room(){
+  const params = useParams();
+
   return(
     <div id="page-room">
       <header>
         <div className='content'>
           <img src={logoImg} alt="Letmeask" />
-          <div>codigo</div>
+          <RoomCode code="1223213214341"/>
         </div>
       </header>  
 

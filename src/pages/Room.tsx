9b1b5fe1f17370aea070f146817665636e5dbbec
Rafael Nóbrega de Lima <rafaelnobregadelima2809@gmail.com>
2021-06-26 16:50:39 -0400

    att

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -3,34 +3,127 @@ import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import { useParams } from 'react-router-dom';
 import '../styles/room.scss';
+import { useState,FormEvent } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+import { useEffect } from 'react';
+
+type FirebaseQuestions = Record<string, {
+  author:{
+    name:string;
+    avatar:string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+}>
+
+  type Question = {
+    id: string;
+    author:{
+      name:string;
+      avatar:string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+
+type RoomParams = {
+  id: string;
+}
 
 export function Room(){
-  const params = useParams();
+  const  { user }  = useAuth();
+  const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([])
+  const roomId = params.id;
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return{
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted: value.isHighLighted,
+          isAnswered: value.isAnswered,
+        }
+      })
+
+      setTitle(databaseRoom.title)
+      setQuestions(parsedQuestions)
+    })
+  }, [roomId]);
+  
+  async function handleSendQuestion(event: FormEvent){
+    event.preventDefault();
+
+    if (newQuestion.trim() === ''){
+      return;
+    }
+    if(!user){
+      throw new Error ('You must be logged in');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighLighted: false,
+      isAnswered: false
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
+  }
 
   return(
     <div id="page-room">
       <header>
         <div className='content'>
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode code="1223213214341"/>
+          <RoomCode code={roomId}/>
         </div>
       </header>  
 
       <main>
         <div className="room-title">
-          <h1>Sala Cu</h1>
-          <span>4 perguntas</span>
+          <h1>Sala {title}</h1>
+          { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
         </div> 
 
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea 
             placeholder="o que você quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
           />
           <div className="form-footer">
-            <span>Para enviar uma pergunta,<button>faça seu login</button>.</span>
-            <Button type="submit">Enviar pergunta</Button>
+            { user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+
+              </div>
+            ) : (
+              <span>Para enviar uma pergunta,<button>faça seu login</button>.</span>
+            )}
+            <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>  
+
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database'; 
-import { Button } from '../components/Button';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
